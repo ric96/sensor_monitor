019698ac9d3f8606bd6824bf2ea4f96741e7a5f1
Sahaj Sarup <sahajsarup@gmail.com>
2014-08-13 19:29:35 +0530

    Considerable reduction/elimination of flickering
    Considerable reduction/elimination of flickering caused due very low refresh intervals

--- sensor.sh
+++ sensor.sh
@@ -1,29 +1,35 @@
+
 #!/bin/bash
 clear
 flag=0
 echo "Do You Want To Enable Voltage Sensors? (y/n)"
 read ans
 echo " "
-echo "Enter Refresh Interval in Seconds:"
+echo "Enter Refresh Interval in Seconds: (less than 0.1 may cause flickering)"
 read x
 while true
 do
+ cpuclk=$(cat /proc/cpuinfo | grep "cpu MHz")
+ cputmp=$(sensors | grep -i -e "Physical" -e "Core")
+ fanspd=$(sensors | grep "fan")
+ hddtmp=$(sensors | grep "temp")
+ inpvlt=$(sensors | grep "in[0-9]")
+ clear
  echo "CPU CLOCK ( MHZ )"
- cat /proc/cpuinfo | grep "cpu MHz"
+ echo "$cpuclk"
  echo " "
  echo "CPU TEMPERATURE:"
- sensors | grep -i -e "Physical" -e "Core"
+ echo "$cputmp"
  echo " "
  echo "FAN SPEED"
- sensors | grep "fan"
+ echo "$fanspd"
  echo " "
- echo "HARDWARE TEMPERATURE"
- sensors | grep "temp"
+ echo "HARD DRIVE TEMPERATURE"
+ echo "$hddtmp"
  echo " "
  if test $ans = "y" ; then
   echo "INPUT VOLTAGES"
-  sensors | grep "in[0-9]"
+  echo "$inpvlt"
  fi
  sleep $x
- clear
 done
